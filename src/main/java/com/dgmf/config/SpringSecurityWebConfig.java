8da6f84ef24b3bded19c180957f89a3148d2b8a5
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-01 11:07:10 +0200

    Refactoring

--- src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
+++ src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
@@ -3,28 +3,21 @@ package com.dgmf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@EnableWebMvc // Enable Spring Security Support for Spring MVC
+// @EnableWebMvc // Enable Spring Security Support for Spring MVC
+@EnableWebSecurity
 public class SpringSecurityWebConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(
-                        (requests) -> requests
-                                .requestMatchers(
-                                        "/home",
-                                        "index",
-                                        "login",
-                                        "register/",
-                                        "signin",
-                                        "signup",
-                                        "register/save",
-                                        "users"
-                                ).permitAll())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
                 .formLogin(
                         form -> form
                                 .loginPage("/login")
