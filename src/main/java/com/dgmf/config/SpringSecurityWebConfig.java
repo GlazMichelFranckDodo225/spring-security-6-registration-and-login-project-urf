8e19775bf60c7b80fe27d4a4aae16b1bbaf3ab84
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-01 15:55:17 +0200

    Configure Spring Security for Logout Endpoint

--- src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
+++ src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -23,6 +24,10 @@ public class SpringSecurityWebConfig {
                                 .loginPage("/login")
                                 .loginProcessingUrl("/login")
                                 .defaultSuccessUrl("/users")
+                                .permitAll()
+                ).logout(
+                        logout -> logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                                 .permitAll()
                 );
 
