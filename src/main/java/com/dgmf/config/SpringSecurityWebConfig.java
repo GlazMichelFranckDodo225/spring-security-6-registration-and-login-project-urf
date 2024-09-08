dc8afd4894ab9db775e4db0a25178f1f2e10ed81
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-08 15:47:08 +0200

    Spring Security 6 Automatically Sets the UserDetailsDervice and PasswordEncoder to AuthenticationManager

--- src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
+++ src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
@@ -1,39 +1,26 @@
 package com.dgmf.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 // @EnableWebMvc // Enable Spring Security Support for Spring MVC
 @EnableWebSecurity
-@RequiredArgsConstructor
+// @RequiredArgsConstructor
 public class SpringSecurityWebConfig {
-    private final UserDetailsService userDetailsService;
+    // private final UserDetailsService userDetailsService;
 
     // "static PasswordEncoder" to Avoid Circular Reference
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    // Set Spring Security User Object and PasswordEncoder to AuthenticationManager
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
-        builder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -61,4 +48,12 @@ public class SpringSecurityWebConfig {
 
         return http.build();
     }
+
+    // Set Spring Security User Object and PasswordEncoder to AuthenticationManager
+    /*@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }*/
 }
