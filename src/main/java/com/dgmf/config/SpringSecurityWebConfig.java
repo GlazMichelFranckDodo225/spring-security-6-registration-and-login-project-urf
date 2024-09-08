efad23335a9756eda9f685447b4b0903cb0b0876
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-08 14:40:15 +0200

    Set Spring Security User Object and PasswordEncoder to AuthenticationManager

--- src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
+++ src/main/java/com/dgmf/config/SpringSecurityWebConfig.java
@@ -1,10 +1,16 @@
 package com.dgmf.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,7 +18,23 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 // @EnableWebMvc // Enable Spring Security Support for Spring MVC
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityWebConfig {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    // Set Spring Security User Object and PasswordEncoder to AuthenticationManager
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder builder) throws Exception {
+        builder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
