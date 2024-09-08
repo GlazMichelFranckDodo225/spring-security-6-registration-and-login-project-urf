dea4769b4070d45e870cd2a6eec7954498675a3d
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-08 14:43:26 +0200

    Encrypt Password Using Spring Security PasswordEncoder

--- src/main/java/com/dgmf/service/impl/UserServiceImpl.java
+++ src/main/java/com/dgmf/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.dgmf.repository.RoleRepository;
 import com.dgmf.repository.UserRepository;
 import com.dgmf.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public void saveUser(UserDto userDto) {
@@ -28,8 +30,9 @@ public class UserServiceImpl implements UserService {
         user.setFirstName(userDto.getFirstName());
         user.setLastName(userDto.getLastName());
         user.setEmail(userDto.getEmail());
-        // Later, Password Must Encrypt Using Spring Security
-        user.setPassword(userDto.getPassword());
+        // user.setPassword(userDto.getPassword());
+        // Encrypt Password Using Spring Security PasswordEncoder
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
 
         Role role = roleRepository.findByName("ROLE_ADMIN");
         // If Role Does Not Exist
